afe32e68b832a5b4086af28bbddc01859df0cb65
ljt <1010437140@qq.com>
2019-02-22 19:48:00 +0800

    v1.0

--- src/main/java/com/baizhi/VoteGitApplication.java
+++ src/main/java/com/baizhi/VoteGitApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VoteGitApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(VoteGitApplication.class, args);
     }
 
